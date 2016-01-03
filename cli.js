1555fddea41a2a5e09641fe870750e4c7a9c5fd8
Paulo McNally <paulomcnally@gmail.com>
2016-01-02 22:52:57 -0600

    first validate empty params

--- cli.js
+++ cli.js
@@ -27,15 +27,15 @@ function padDigits(number, digits) {
     return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
 }
 
+if (cli.input.length === 0) {
+  console.error('Please specify a tuc number');
+  process.exit(1);
+}
+
 if (cli.input[0].toString().length < 8) {
   var card_number = padDigits(cli.input[0].toString(), 8);
 } else {
   var card_number = cli.input[0].toString();
-}
-
-if (cli.input.length === 0) {
-  console.error('Please specify a tuc number');
-  process.exit(1);
 }
 
 if (cli.flags.t) {
@@ -47,4 +47,3 @@ if (cli.flags.t) {
 tuc.getBalance(card_number, function(balance) {
     console.log(balance);
 });
-
